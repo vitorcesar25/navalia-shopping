ce39c3c147047a893123c90baaaeac512421c43b
Vitor Cesar <vitorcesar@vetalfa.com>
2025-01-13 20:42:57 -0300

    chore(services): created a request middleware to check parameters from request and raise BAD Request responses on parameters check fail.

---
+++ services/shared/middlewares/requestVerificationMiddleware.js
@@ -0,0 +1,23 @@
+const { validationResult, checkSchema } = require('express-validator');
+
+/**
+ * Middleware to validate request parameters, query, body, and headers.
+ *
+ * @param {Object} schema - Validation schema using express-validator.
+ * @returns {Function[]} Array of middleware functions.
+ */
+const validate = (schema) => {
+    const validations = checkSchema(schema);
+    return [
+        ...validations,
+        (req, res, next) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+            next();
+        },
+    ];
+};
+
+module.exports = validate;
