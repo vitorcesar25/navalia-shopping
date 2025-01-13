0f528c9f541b30b032685ed98ba476640d06b357
Vitor Cesar <vitorcesar@vetalfa.com>
2025-01-13 20:42:14 -0300

    chore(services): Created Error Middleware to handle exceptions from services calls, created a log interface and a pattern for http status error codes.

---
+++ services/shared/middlewares/errorHandlerMiddleware.js
@@ -0,0 +1,27 @@
+const { CustomError } = require('../errors/CustomErrors');
+const { logError, logRequest, logSuccessResponse } = require("../utils/logger");
+/**
+ * Middleware to handle errors in asynchronous routes.
+ * Recognizes custom errors and responds with the appropriate status code.
+ * 
+ * @param {Function} fn - The asynchronous route handler function.
+ * @returns {Function} A wrapped route handler with error handling.
+ */
+const errorHandlerMiddleware = (fn) => {
+    return async (req, res, next) => {
+        logRequest(req);
+        try {
+            await fn(req, res, next);
+            logSuccessResponse(req, res);
+        } catch (error) {
+            logError(req, error);
+            if (error instanceof CustomError) {
+                res.status(error.statusCode).json({ error: error.message });
+            } else {
+                res.status(500).json({ error: "Internal server error" });
+            }
+        }
+    };
+};
+
+module.exports = errorHandlerMiddleware;
