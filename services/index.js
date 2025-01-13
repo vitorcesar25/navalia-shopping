1fc884dad836db8d6521150b769895a986bfcbc1
Vitor Cesar <vitorcesar@vetalfa.com>
2025-01-13 20:31:51 -0300

    chore(services): Created firebase functions entrypoint and configuration files.

---
+++ services/index.js
@@ -0,0 +1,22 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const admin = require('firebase-admin');
+const cors = require('cors');
+require('dotenv').config();
+
+
+if (!admin.apps.length) {
+    admin.initializeApp({
+        projectId: process.env.FIRESTORE_PROJECT_ID,
+    });
+}
+
+const app = express();
+app.use(cors({
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
+
+exports.api = functions.https.onRequest(app);
