448ed6744a6234e8ebac97a815489c6532468a68
Vitor Cesar <vitorcesar@vetalfa.com>
2025-01-13 21:22:30 -0300

    feat(frontend cart) Added cart button to header

---
+++ frontend/src/components/CartButtonComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-btn icon color="primary" @click="toggleCartDialog" title="Open Cart">
+    <v-icon>mdi-cart</v-icon>
+  </v-btn>
+</template>
+
+<script>
+import { mapMutations } from "vuex";
+
+export default {
+  name: "CartButtonComponent",
+  methods: {
+    ...mapMutations("cart", ["toggleCartDialog"]),
+  },
+};
+</script>
--- frontend/src/components/HeaderComponent.vue
+++ frontend/src/components/HeaderComponent.vue
@@ -2,16 +2,19 @@
   <v-app-bar app>
     <v-toolbar-title>Navalia Shopping</v-toolbar-title>
     <v-spacer></v-spacer>
+    <CartButtonComponent />
     <LoginComponent />
   </v-app-bar>
 </template>
 
 <script>
 import LoginComponent from "./LoginComponent.vue";
+import CartButtonComponent from "./CartButtonComponent.vue";
 export default {
   name: "HeaderComponent",
   components: {
     LoginComponent,
+    CartButtonComponent,
   },
 };
 </script>
