461c51932bd069794f605fdff7ea153e6562322c
Vitor Cesar <vitorcesar@vetalfa.com>
2025-01-13 22:40:19 -0300

    fix(services cart) Rename output domain conventions

--- services/cart/src/application/getCart.usecase.js
+++ services/cart/src/application/getCart.usecase.js
@@ -1,6 +1,5 @@
 const cartRepository = require('../infrastructure/repositories/cart.repository.js');
 const Cart = require('../domain/Cart');
-const CartOutputDTO = require('../dto/CartOutputDTO');
 
 /**
  * Fetches the cart data for a given user and enriches it with product metadata.
@@ -9,7 +8,7 @@ const CartOutputDTO = require('../dto/CartOutputDTO');
  * @param {Function} fetchProductMetadata - A function to fetch product metadata. 
  * It should accept an array of product IDs and return metadata for those products.
  * 
- * @returns {Promise<CartOutputDTO>} A DTO containing the enriched cart items and the total cost.
+ * @returns {Promise<Cart>} A class containing the enriched cart items and the total cost.
  * 
  * @throws {Error} If there is an issue fetching cart data or product metadata.
  */
@@ -18,7 +17,9 @@ const getCartByUserId = async (userId, getProductsMetadata) => {
     const cartData = await cartRepository.getCartByUserId(userId);
 
     if (!cartData || cartData.items.length === 0) {
-        return new CartOutputDTO({ items: [], total: 0 });
+        const cart = new Cart(userId, []);
+        cart.calculateTotal();
+        return cart;
     }
 
     const cart = new Cart(userId, cartData.items);
@@ -27,7 +28,7 @@ const getCartByUserId = async (userId, getProductsMetadata) => {
     cart.enrichItems(productMetadata);
     cart.calculateTotal();
 
-    return new CartOutputDTO(cart);
+    return cart;
 };
 
 module.exports = { getCartByUserId };
